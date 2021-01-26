54ff45b87b49e574e2260cf147b126f17015d58b
rozinhilmi <69004866+rozinhilmi@users.noreply.github.com>
2021-01-26 18:52:17 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -63,8 +63,7 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
+window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
